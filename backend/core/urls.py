1a24efc1da1b6af8b259356858677a8162cbb4bf
Ritheesh <you@example.com>
2026-02-03 19:04:45 +0530

    Add backend root endpoint and Netlify SPA redirects

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": "Backend is running",
+        "project": "Chemical Equipment Parameter Visualizer",
+        "backend": "Django + Django REST Framework"
+    })
 
 urlpatterns = [
+    path("", home),                  # 👈 root URL
     path("admin/", admin.site.urls),
     path("api/", include("equipment.urls")),
 ]
